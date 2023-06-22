56afe22c20cf5f7e27dfe94afe0db4106ede6664
SimonTheFool <chengzilin2011@gmail.com>
2023-06-23 00:30:22 +0800

    Initial Commit

---
+++ SimonPlayground/SimonPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonPlaygroundApp.swift
+//  SimonPlayground
+//
+//  Created by 程子霖 on 23/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimonPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimonPlaygroundUITests/SimonPlaygroundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SimonPlaygroundUITestsLaunchTests.swift
+//  SimonPlaygroundUITests
+//
+//  Created by 程子霖 on 23/6/2023.
+//
+
+import XCTest
+
+final class SimonPlaygroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
